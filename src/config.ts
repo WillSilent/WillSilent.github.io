ab2d7283a419def97a9d5db42c1ce3ff2bc032b8
Will Liu <will990211@163.com>
2024-02-08 22:38:06 -0600

    ignore to submit some files.

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Wen Liu | Personal Website'
+export const SITE_DESCRIPTION = "Wen Liu's Blog, CV, and Project Description."
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
